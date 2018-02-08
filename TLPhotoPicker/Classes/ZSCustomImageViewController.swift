a9c8f6b0d462d7105b3081754c7087af705d2e64
rizwan.ahmed@zohocorp.com <rizwan.ahmed@zohocorp.com>
2018-02-08 08:41:44 +0530

    Added support for Objective C project integration.

---
+++ TLPhotoPicker/Classes/ZSCustomImageViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ZSCustomImageViewController.swift
+//  Streams
+//
+//  Created by Rizwan Ahmed A on 18/12/17.
+//  Copyright © 2017 ZOHOCORP. All rights reserved.
+//
+
+import UIKit
+
+class ZSCustomPhotoPickerViewController: TLPhotosPickerViewController {
+    override func makeUI() {
+        super.makeUI()
+        self.customNavItem.leftBarButtonItem = UIBarButtonItem.init(barButtonSystemItem: .stop, target: nil, action: #selector(customAction))
+        
+    }
+    @objc func customAction() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func doneButtonTap() {
+        super.doneButtonTap()
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+    }
+}
+
---
+++ TLPhotoPicker/Classes/ZSImagePickerWrapperClassViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ZSImagePickerWrapperClassViewController.swift
+//  Streams
+//
+//  Created by Rizwan Ahmed A on 18/12/17.
+//  Copyright © 2017 ZOHOCORP. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+@objc protocol ZSImagePickerWrapperClassDelegate {
+    
+    func picked(images:[PHAsset]?)
+    func getSelectedImagesCount(count :Int64)
+    
+}
+
+extension ZSImagePickerWrapperClassDelegate{
+    public func picked(images:[PHAsset]?){}
+    public func imagesSelected(count :Int64){}
+    
+}
+
+@objc class ZSImagePickerWrapperClassViewController: NSObject {
+    
+    var selectedAssets = [TLPHAsset]()
+    
+    
+    @objc public var  imageViewController : ZSCustomPhotoPickerViewController?
+    
+    @objc weak var imagePickerDelegate: ZSImagePickerWrapperClassDelegate?
+
+    override init() {
+        super.init()
+        createImagePickerView()
+    }
+    
+    //MARK : Local methdos
+    
+    func createImagePickerView(){
+        imageViewController = ZSCustomPhotoPickerViewController()
+        imageViewController!.delegate = self
+
+        var configure = TLPhotosPickerConfigure()
+        configure.numberOfColumn = 4
+        configure.maxSelectedAssets = 7
+        
+//        if #available(iOS 10.2, *) {
+//            configure.cameraCellNibSet = (nibName: "CustomCameraCell", bundle: Bundle.main)
+//        }
+        imageViewController!.configure = configure
+
+        imageViewController!.selectedAssets = self.selectedAssets
+        
+    }
+    
+    //MARK : Instance Methods
+    
+    //Diss miss without Navigation controller.
+    
+    @objc public func imagePickingDone(){
+        imageViewController!.dismissWithoutNavigationController()
+    }
+    
+    // Use this method to remove the photos programmatically. Example - When swiping the selected images, you can call this image to deselect the images from the collection view of TLPhotosPickerViewController.
+    
+    @objc public func removeImageFor(key : String){
+        imageViewController?.removeImage(ForKey: key)
+    }
+    
+    deinit {
+        print("deinit success")
+    }
+
+    
+}
+
+extension ZSImagePickerWrapperClassViewController : TLPhotosPickerViewControllerDelegate {
+    
+    
+    func dismissPhotoPicker(withTLPHAssets: [TLPHAsset]) {
+        self.selectedAssets = withTLPHAssets
+    }
+    func dismissPhotoPicker(withPHAssets: [PHAsset]) {
+        
+        self.imagePickerDelegate?.picked(images: withPHAssets)
+        
+    }
+    func photoPickerDidCancel() {
+        
+    }
+    func dismissComplete() {
+        
+        
+    }
+    
+    func didExceedMaximumNumberOfSelection(picker: TLPhotosPickerViewController) {
+        
+    }
+    func didStartSelectingImage(WithCount: Int64) {
+        self.imagePickerDelegate?.getSelectedImagesCount(count: WithCount)
+    }
+    
+
+}
+
